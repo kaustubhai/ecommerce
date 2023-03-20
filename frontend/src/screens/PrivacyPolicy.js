cd2903b4564ff92b2dfd757a878bd4cad5716dc3
kaustubhai <kaustubh229@gmail.com>
2023-03-20 14:19:49 +0530

    Added "our" top products

--- frontend/src/screens/PrivacyPolicy.js
+++ frontend/src/screens/PrivacyPolicy.js
@@ -157,7 +157,7 @@ const PrivacyPolicy = () => {
         tracking.
       </p>
 
-      <h1 className="mt-4 text-center">Top Products</h1>
+      <h1 className="mt-4 text-center">Our Top Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
